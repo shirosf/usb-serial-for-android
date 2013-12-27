b200fd39468dc4c034ad80551d2486205b113f27
Shiro Ninomiya <shiron@snino.com>
2013-12-28 07:48:13 +0900

    this works to read data

--- UsbSerialExamples/src/com/hoho/android/usbserial/examples/SerialConsoleActivity.java
+++ UsbSerialExamples/src/com/hoho/android/usbserial/examples/SerialConsoleActivity.java
@@ -25,16 +25,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ScrollView;
 import android.widget.TextView;
+import android.view.View;
+import android.os.SystemClock;
 
 import com.hoho.android.usbserial.driver.UsbSerialDriver;
 import com.hoho.android.usbserial.util.HexDump;
-import com.hoho.android.usbserial.util.SerialInputOutputManager;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.Arrays;
 
 /**
  * Monitors a single {@link UsbSerialDriver} instance, showing all data
@@ -60,44 +61,18 @@ public class SerialConsoleActivity extends Activity {
 
     private TextView mTitleTextView;
     private TextView mDumpTextView;
-    private ScrollView mScrollView;
-
-    private final ExecutorService mExecutor = Executors.newSingleThreadExecutor();
-
-    private SerialInputOutputManager mSerialIoManager;
-
-    private final SerialInputOutputManager.Listener mListener =
-            new SerialInputOutputManager.Listener() {
-
-        @Override
-        public void onRunError(Exception e) {
-            Log.d(TAG, "Runner stopped.");
-        }
-
-        @Override
-        public void onNewData(final byte[] data) {
-            SerialConsoleActivity.this.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    SerialConsoleActivity.this.updateReceivedData(data);
-                }
-            });
-        }
-    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.serial_console);
         mTitleTextView = (TextView) findViewById(R.id.demoTitle);
-        mDumpTextView = (TextView) findViewById(R.id.consoleText);
-        mScrollView = (ScrollView) findViewById(R.id.demoScroller);
+        mDumpTextView = (TextView) findViewById(R.id.show_message);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        stopIoManager();
         if (sDriver != null) {
             try {
                 sDriver.close();
@@ -131,36 +106,24 @@ public class SerialConsoleActivity extends Activity {
                 return;
             }
             mTitleTextView.setText("Serial device: " + sDriver.getClass().getSimpleName());
+
+            try {
+		if(send_wait_ack(TCOM_DOSERATE)!=0){
+		    mDumpTextView.append("can't send doserate command\n");
+		    return;
+		}
+		if(send_wait_ack(TCOM_MES30SEC)!=0){
+		    mDumpTextView.append("can't send measurement time parameter\n");
+		    return;
+		}
+		tcom_state=1;
+		mDumpTextView.append("Initialized\n");
+            } catch (IOException e) {
+		    mDumpTextView.append("IOException\n");
+		    return;
+	    }
+	    
         }
-        onDeviceStateChange();
-    }
-
-    private void stopIoManager() {
-        if (mSerialIoManager != null) {
-            Log.i(TAG, "Stopping io manager ..");
-            mSerialIoManager.stop();
-            mSerialIoManager = null;
-        }
-    }
-
-    private void startIoManager() {
-        if (sDriver != null) {
-            Log.i(TAG, "Starting io manager ..");
-            mSerialIoManager = new SerialInputOutputManager(sDriver, mListener);
-            mExecutor.submit(mSerialIoManager);
-        }
-    }
-
-    private void onDeviceStateChange() {
-        stopIoManager();
-        startIoManager();
-    }
-
-    private void updateReceivedData(byte[] data) {
-        final String message = "Read " + data.length + " bytes: \n"
-                + HexDump.dumpHexString(data) + "\n\n";
-        mDumpTextView.append(message);
-        mScrollView.smoothScrollTo(0, mDumpTextView.getBottom());
     }
 
     /**
@@ -179,16 +142,22 @@ public class SerialConsoleActivity extends Activity {
     private final byte STX=0x02;
     private final byte ETX=0x03;
     private final byte ACK=0x06;
-    private final static char[] HEX_DIGITS = {
+    private final static byte[] HEX_DIGITS = {
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
     };
-    private Boolean wait_ack = false;
+    private static final int RW_WAIT_MILLIS = 10000;
+    private static final byte[] TCOM_DOSERATE = {'8','B','0'};
+    private static final byte[] TCOM_MES30SEC = {'8','0','2'};
+    private static final byte[] TCOM_MES10SEC = {'8','0','1'};
+    private static final byte[] TCOM_MES03SEC = {'8','0','0'};
+    private static final byte[] TCOM_SPECTRUM = {'8','B','1'};
+    private int tcom_state=0;
 
-    private int send_data(byte[] data)
+    private int send_data(byte[] data) throws IOException 
     {
 	int parity=0;
 	byte[] sdata;
-	if(!mSerialIoManager) return -1;
+	if(sDriver==null) return -1;
 
 	sdata=new byte[data.length+4];
 	sdata[0]=STX;
@@ -197,40 +166,107 @@ public class SerialConsoleActivity extends Activity {
 	    parity^=data[i];
 	}
 	sdata[data.length+1]=ETX;
-	sdata[data.length+2]=HEX_DIGITS((parity>>4)&0x0F);
-	sdata[data.length+3]=HEX_DIGITS(parity&0x0F);
-	mSerialIoManager.writeAsync(sdata);
-	wait_ack=true;
+	sdata[data.length+2]=HEX_DIGITS[(parity>>4)&0x0F];
+	sdata[data.length+3]=HEX_DIGITS[parity&0x0F];
+	mDumpTextView.append(HexDump.dumpHexString(sdata)+'\n');
+	return sDriver.write(sdata, RW_WAIT_MILLIS);
+    }
+
+    private int send_wait_ack(byte[] data) throws IOException
+    {
+	byte[] recd=new byte[1];
+        if(send_data(data)<0) return -1;
+        if(sDriver.read(recd, RW_WAIT_MILLIS)!=1) return -1;
+        if(recd[0]!=ACK) return -1;
 	return 0;
     }
 
-    private byte[] check_rec_data(byte[] data)
+    private byte[] rec_data_tout() throws IOException
     {
-	byte[] res;
+	byte[] recd=new byte[128];
+        byte[] rss=new byte[2];
 	int parity=0;
-	int len=0;
-	if(data.length<4) return null;
-	if(data[0]!=STX) return null;
-	for(byte d : data){
-	    if(d==ETX) break;
-	    parity ^= d;
-	    len++;
+	int rstate=0;
+	int reclen;
+
+	// all data comes at one time
+	if((reclen=sDriver.read(recd, RW_WAIT_MILLIS))==0) {
+	    mDumpTextView.append("Timed Out ro receive data\n");
+	    return null;
 	}
-	if(len>data.length-3) return null;
-	int rb=Integer.parseInt(new String(data, len+1,2),16);
-	if(rb!=parity) return null;
-	return Arrays.copyOfRange(data,1,len);
+	if(recd[0]!=STX){
+	    mDumpTextView.append("No STX\n");
+	    return null;
+	}
+        for(int i=0;i<reclen;i++){
+	    switch(rstate){
+	    case 0:
+		if(recd[i]!=STX) break;
+		rstate=1;
+		break;
+	    case 1:
+		if(recd[i]==ETX){
+		    rstate=2;
+		    break;
+		}
+		parity^=recd[i];
+		break;
+	    case 2:
+		rss[0]=recd[i];
+		rstate=3;
+		break;
+	    case 3:
+		rss[1]=recd[i];
+		int rb=Integer.parseInt(new String(rss, 0,2),16);
+		if(rb!=parity) {
+		    mDumpTextView.append("parity doesn't match\n");
+		    return null;
+		}
+		return Arrays.copyOfRange(recd,1,i-2); // parity matched
+	    }
+	}
+	mDumpTextView.append("Garbage?\n");
+	return null;
+    }
+
+    private int get_dose_rate(byte[] data)
+    {
+	mDumpTextView.append(HexDump.dumpHexString(data)+'\n');
+	if(data[0]!='0' || data[1]!='1') return -1;
+	return Integer.parseInt(new String(data, 2, data.length-2),16);
     }
 
     
 
     /** Called when the user clicks the Read button */
     public void readData(View view) {
-        if (mSerialIoManager != null) {
-	    mDumpTextView.append("Read one data\n");
+    byte[] rdata=null;
+	int doserate;
+        if (tcom_state==1) {
+            try {
+		mDumpTextView.append("Read one data\n");
+		if(send_data(new byte[] {'0','1'})<0){
+		    mDumpTextView.append("send_data 01 error\n");
+		    return;
+		}
+		rdata=rec_data_tout();
+		if(rdata==null){
+		    mDumpTextView.append("rec_data_tout error\n");
+		    return;
+		}
+		doserate=get_dose_rate(rdata);
+		if(send_data(new byte[] {'0','2'})<0){
+		    mDumpTextView.append("send_data 02 error\n");
+		    return;
+		}
+		mDumpTextView.append(String.format("%d\n",doserate));
+		return;
+            } catch (IOException e) {
+		mDumpTextView.append("IOException\n");
+	    }
+
 	}else{
 	    mDumpTextView.append("Not Connected\n");
 	}
-        mScrollView.smoothScrollTo(0, mDumpTextView.getBottom());
     }
 }
